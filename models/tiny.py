9cc68597487e22aa7c21673a54e41f51d7c96c55
developer0hye <developer.0hye@gmail.com>
2021-01-28 01:11:00 +0900

    change weight initialization method

--- models/tiny.py
+++ models/tiny.py
@@ -36,7 +36,7 @@ class YOLOv3TinyBackbone(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
