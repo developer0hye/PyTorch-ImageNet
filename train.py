a12dbfbc89299ecb9e2d9281fb7ad50856a1f804
Yonghye Kwon <developer.0hye@gmail.com>
2021-07-23 18:55:41 +0900

    faster setting

--- train.py
+++ train.py
@@ -50,8 +50,8 @@ def set_random_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed) # if use multi-GPU
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
     np.random.seed(seed)
     random.seed(seed)
 
